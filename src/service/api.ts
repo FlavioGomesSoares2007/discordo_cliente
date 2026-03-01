1b67d3835893bee15a8dae4199e34c2508b8df99
FLAVIO GOMES SOARES <FlavioGomesSoares2007@gmail.com>
2026-03-01 18:29:59 -0300

    aaaaaaaaa
    aaaaaaaaaa

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL:"https://discordo-api.onrender"
+    baseURL:"https://discordo-api.onrender.com"
 })
 api.interceptors.request.use((config) => {
     
