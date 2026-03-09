07f1acb523c5cacfff5afd0f7dbe4f0d94a8d149
FLAVIO GOMES SOARES <FlavioGomesSoares2007@gmail.com>
2026-03-09 10:11:42 -0300

    msgs

--- src/service/socket.ts
+++ src/service/socket.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const URL = " https://discordo-api.onrender.com";
+const URL = "https://discordo-api.onrender.com";
 
 export const socket = io(URL, {
   autoConnect: false,
